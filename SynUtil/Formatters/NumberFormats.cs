d124cf1ca5452f443ad5f8a6dba0b9a1c2dc698f
SyntaxCheck <justin.wolbeck@gmail.com>
2020-05-24 22:57:46 -0500

    Added number format class

---
+++ SynUtil/Formatters/NumberFormats.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynUtil.Formatters
+{
+    class NumberFormats
+    {
+        private string LargeNumberToReadableText(long num)
+        {
+            string formatted = String.Empty;
+
+            if (num < 999)
+            {
+                formatted = num.ToString();
+            }
+            else if (num >= 1000 && num < 10000)
+            {
+                formatted = Math.Round(num / 1000.0, 2).ToString("#.##") + "k";
+            }
+            else if (num >= 10000 && num < 999999)
+            {
+                formatted = Math.Round(num / 1000.0, 0).ToString("#.##") + "k";
+            }
+            else if (num >= 1000000 && num < 10000000)
+            {
+                formatted = Math.Round(num / 1000000.0, 2).ToString("#.##") + "m";
+            }
+            else if (num >= 10000000 && num < 999999999)
+            {
+                formatted = Math.Round(num / 1000000.0, 0).ToString("#.##") + "m";
+            }
+            else if (num >= 1000000000 && num < 10000000000)
+            {
+                formatted = Math.Round(num / 1000000000.0, 2).ToString("#.##") + "b";
+            }
+            else if (num >= 10000000000 && num < 999999999999)
+            {
+                formatted = Math.Round(num / 1000000000.0, 0).ToString("#.##") + "b";
+            }
+            else if (num >= 1000000000000 && num < 10000000000000)
+            {
+                formatted = Math.Round(num / 1000000000000.0, 2).ToString("#.##") + "t";
+            }
+            else if (num >= 10000000000000 && num < 999999999999999)
+            {
+                formatted = Math.Round(num / 1000000000000.0, 0).ToString("#.##") + "t";
+            }
+            else
+            {
+                formatted = num.ToString();
+            }
+
+            return formatted;
+        }
+    }
+}
