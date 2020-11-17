27a140e830241bede9839184abb2df3b4eededfc
SyntaxCheck <justin.wolbeck@gmail.com>
2020-11-17 00:30:16 -0600

    New formatter for TimeSpan and added some new NumberFormat functions

--- SynUtil/Formatters/NumberFormats.cs
+++ SynUtil/Formatters/NumberFormats.cs
@@ -51,5 +51,117 @@ namespace SynUtil.Formatters
 
             return formatted;
         }
+        public static string NumFormatLargeAndDecimalsCurrency(double num)
+        {
+            string rtn = NumFormatLargeAndDecimals(num, true);
+            if (rtn != "N/A")
+                return "$" + rtn;
+            else
+                return rtn;
+        }
+        public static string NumFormatLarge(double num)
+        {
+            return NumFormatLargeAndDecimals(num, false);
+        }
+        public static string NumFormatLargeAndDecimals(double num)
+        {
+            return NumFormatLargeAndDecimals(num, true);
+        }
+        public static string NumFormatLargeAndDecimals(double num, bool decimals)
+        {
+            string formatted = String.Empty;
+            bool isNegative = false;
+
+            if (num == -1d)
+            {
+                return "N/A";
+            }
+
+            if (num < 0)
+            {
+                num = Math.Abs(num);
+                isNegative = true;
+            }
+
+            if (num == 0)
+            {
+                formatted = num.ToString();
+            }
+            else if (num < 999)
+            {
+                if (decimals)
+                {
+                    if (num >= 10)
+                    {
+                        formatted = Math.Round(num, 0).ToString();
+                    }
+                    else if (num >= 1)
+                    {
+                        formatted = Math.Round(num, 2).ToString("#.00");
+                    }
+                    else if (num >= 0.01)
+                    {
+                        formatted = Math.Round(num, 3).ToString("0.000");
+                    }
+                    else if (num >= 0.001)
+                    {
+                        formatted = Math.Round(num, 4).ToString("0.0000");
+                    }
+                    else if (num >= 0.0001)
+                    {
+                        formatted = Math.Round(num, 5).ToString("0.00000");
+                    }
+                    else
+                    {
+                        formatted = Math.Round(num, 6).ToString("0.000000");
+                    }
+                }
+                else
+                {
+                    formatted = num.ToString();
+                }
+            }
+            else if (num >= 1000 && num < 10000)
+            {
+                formatted += Math.Round(num / 1000.0, 2).ToString("#.##") + "k";
+            }
+            else if (num >= 10000 && num < 999999)
+            {
+                formatted += Math.Round(num / 1000.0, 0).ToString("#.##") + "k";
+            }
+            else if (num >= 1000000 && num < 10000000)
+            {
+                formatted += Math.Round(num / 1000000.0, 2).ToString("#.##") + "m";
+            }
+            else if (num >= 10000000 && num < 999999999)
+            {
+                formatted += Math.Round(num / 1000000.0, 0).ToString("#.##") + "m";
+            }
+            else if (num >= 1000000000 && num < 10000000000)
+            {
+                formatted += Math.Round(num / 1000000000.0, 2).ToString("#.##") + "b";
+            }
+            else if (num >= 10000000000 && num < 999999999999)
+            {
+                formatted += Math.Round(num / 1000000000.0, 0).ToString("#.##") + "b";
+            }
+            else if (num >= 1000000000000 && num < 10000000000000)
+            {
+                formatted += Math.Round(num / 1000000000000.0, 2).ToString("#.##") + "t";
+            }
+            else if (num >= 10000000000000 && num < 999999999999999)
+            {
+                formatted += Math.Round(num / 1000000000000.0, 0).ToString("#.##") + "t";
+            }
+            else
+            {
+                formatted += num.ToString();
+            }
+
+            if (isNegative)
+                formatted = "-" + formatted;
+
+            return formatted;
+        }
     }
 }
---
+++ SynUtil/Formatters/TimeSpanFormatter.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace SynUtil.Formatters
+{
+    public static class TimeSpanFormatter
+    {
+        public static string TimeSpanFormattedFull(TimeSpan ts)
+        {
+            string timespFormatted = String.Empty;
+
+            if (ts.Days > 0)
+            {
+                timespFormatted += ts.Days.ToString() + " day" + AppendS(ts.Days);
+            }
+            if (ts.Days > 0 || ts.Hours > 0)
+            {
+                if (!String.IsNullOrEmpty(timespFormatted))
+                {
+                    timespFormatted += ", ";
+                }
+
+                timespFormatted += ts.Hours.ToString() + " hour" + AppendS(ts.Hours);
+            }
+            if (ts.Days > 0 || ts.Hours > 0 || ts.Minutes > 0)
+            {
+                if (!String.IsNullOrEmpty(timespFormatted))
+                {
+                    timespFormatted += ", ";
+                }
+
+                timespFormatted += ts.Minutes.ToString() + " minute" + AppendS(ts.Minutes);
+            }
+            if (ts.Days > 0 || ts.Hours > 0 || ts.Minutes > 0 || ts.Seconds > 0)
+            {
+                if (!String.IsNullOrEmpty(timespFormatted))
+                {
+                    timespFormatted += ", ";
+                }
+
+                timespFormatted += ts.Seconds.ToString() + " second" + AppendS(ts.Seconds);
+            }
+            if (ts.Days > 0 || ts.Hours > 0 || ts.Minutes > 0 || ts.Seconds > 0 || ts.Milliseconds > 0)
+            {
+                if (!String.IsNullOrEmpty(timespFormatted))
+                {
+                    timespFormatted += ", ";
+                }
+
+                timespFormatted += ts.Milliseconds.ToString() + " millisecond" + AppendS(ts.Milliseconds);
+            }
+
+            return timespFormatted;
+        }
+        public static string TimeSpanHighestDecimal(TimeSpan timeSpan)
+        {
+            string unit = String.Empty;
+            double num = 0;
+
+            if (timeSpan.TotalDays >= 1)
+            {
+                unit = "day";
+                num = timeSpan.TotalDays;
+            }
+            else if (timeSpan.TotalHours >= 1)
+            {
+                unit = "hour";
+                num = timeSpan.TotalHours;
+            }
+            else if (timeSpan.TotalMinutes >= 1)
+            {
+                unit = "minute";
+                num = timeSpan.TotalMinutes;
+            }
+            else if (timeSpan.TotalSeconds >= 1)
+            {
+                unit = "second";
+                num = timeSpan.TotalSeconds;
+            }
+            else if (timeSpan.TotalMilliseconds >= 1)
+            {
+                unit = "millisecond";
+                num = timeSpan.TotalMilliseconds;
+            }
+
+            if (num > 0)
+            {
+                return Math.Round(num, 2).ToString() + " " + unit + AppendS(num);
+            }
+
+            return String.Empty;
+        }
+        private static string AppendS(double number)
+        {
+
+            if (number != 1.0)
+                return "s";
+
+            return String.Empty;
+        }
+    }
+}
